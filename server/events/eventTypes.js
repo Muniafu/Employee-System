e658bf370d84df11bf94c379b1dc0c025ca7ea75
Muniafu <elambaraka2019@gmail.com>
2026-05-12 15:56:16 +0300

    feat: add event bus and event types for handling application events

---
+++ server/events/eventTypes.js
@@ -0,0 +1,25 @@
+module.exports = {
+  LEAVE_REQUESTED:
+    'leave.requested',
+
+  LEAVE_APPROVED:
+    'leave.approved',
+
+  LEAVE_REJECTED:
+    'leave.rejected',
+
+  PAYROLL_FINALIZED:
+    'payroll.finalized',
+
+  EMPLOYEE_CREATED:
+    'employee.created',
+
+  EMPLOYEE_DEACTIVATED:
+    'employee.deactivated',
+
+  POLICY_PUBLISHED:
+    'policy.published',
+
+  SYSTEM_NOTIFICATION:
+    'system.notification',
+};
