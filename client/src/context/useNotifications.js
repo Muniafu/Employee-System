643d27c0a0340c97f25c35cddafdd0ada2c14e03
Muniafu <elambaraka2019@gmail.com>
2026-05-12 16:00:47 +0300

    feat: add NotificationContext and useNotifications hook for notification management

---
+++ client/src/context/useNotifications.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+
+import NotificationContext
+  from './NotificationContextObject';
+
+export const useNotifications =
+  () => {
+    const context =
+      useContext(
+        NotificationContext
+      );
+
+    if (!context) {
+      throw new Error(
+        'useNotifications must be used within NotificationProvider'
+      );
+    }
+
+    return context;
+  };
